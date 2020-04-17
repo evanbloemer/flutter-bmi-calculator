6062c7711c904c801cfe2f2fe757ea4cc07180e0
Evan Bloemer <evan@multingo.com>
2020-04-17 14:02:19 +0900

    First commit

---
+++ lib/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const double kIconSize = 80.0;
+const Color kIconColor = Colors.white;
+const double kLabelSize = 18.0;
+const Color kLabelColor = Color(0xFF8D8E98);
+
+const Color kActiveCardColor = Color(0xFF1D1E33);
+const Color kInactiveCardColor = Color(0xFF111328);
+const Color kBottomBarColor = Color(0xFFEB1555);
+const double kBottomBarHeight = 80.0;
+
+const double kBorderRadius = 10.0;
+
+const TextStyle kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const TextStyle kLabelTextStyle = TextStyle(
+  fontSize: kLabelSize,
+  color: kLabelColor,
+);
---
+++ lib/reusable_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  final Widget child;
+  final Function onTap;
+
+  ReusableCard({@required this.color, this.child, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: child,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(kBorderRadius),
+          color: color,
+        ),
+        margin: EdgeInsets.all(10.0),
+      ),
+    );
+  }
+}
